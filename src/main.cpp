86f6c8ffb5760ab4dbf1f25021f9ea9294109656
MickeMakaron <mikael.hernvall@gmail.com>
2018-05-26 12:28:04 +0200

    Add glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,73 @@
 #include <iostream>
 
+#include <GL/glew.h>
 #include <SDL.h>
 
 int main(int, char**) {
-   std::cout << "Hello, world!\n";
+// Setup SDL
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+  {
+    printf("Error: %s\n", SDL_GetError());
+    return -1;
+  }
+
+  // Setup window
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
+                      SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_DisplayMode current;
+  SDL_GetCurrentDisplayMode(0, &current);
+  SDL_Window *window =
+      SDL_CreateWindow("ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 1280, 720,
+                       SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+  SDL_GL_SetSwapInterval(1); // Enable vsync
+  glewInit();
+
+  glEnable(GL_DEPTH_TEST);
 
 
-   return 0;
+  bool done = false;
+  while (!done)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+      case SDL_QUIT:
+        done = true;
+        break;
+      case SDL_MOUSEBUTTONDOWN:
+        if (event.button.button == SDL_BUTTON_RIGHT)
+        break;
+      case SDL_KEYDOWN:
+      {
+        break;
+      }
+      case SDL_KEYUP:
+      {
+        break;
+      }
+      }
+    }
+   
+    // Rendering
+    glViewport(0, 0, 800, 800);
+    glClearColor(255, 0, 255, 255);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DeleteContext(gl_context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
