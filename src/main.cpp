800f986926ba00f29dae9f095e69c75061d27166
MickeMakaron <mikael.hernvall@gmail.com>
2018-05-26 12:37:24 +0200

    Draw square

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,154 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 #include <GL/glew.h>
 #include <SDL.h>
+
+
+void printShaderLog(GLuint shader)
+{
+  int logLength = 0;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+	if(logLength > 0)
+  {
+		std::vector<char> msg(logLength + 1);
+		glGetShaderInfoLog(shader, logLength, NULL, &msg[0]);
+		std::cout << msg.data() << std::endl;
+	}
+}
+
+void printProgramLog(GLuint prog)
+{
+  int logLength = 0;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &logLength);
+	if(logLength > 0)
+  {
+		std::vector<char> msg(logLength + 1);
+		glGetProgramInfoLog(prog, logLength, NULL, &msg[0]);
+		std::cout << msg.data() << std::endl;
+	}
+}
+
+void compileShader(GLuint shader, const char* src)
+{
+  glShaderSource(shader, 1, &src, NULL);
+	glCompileShader(shader);
+  printShaderLog(shader);
+}
+
+void throwGlError(const char* msg)
+{
+  GLenum error = glGetError();
+  if (error != GL_NO_ERROR)
+  {
+    std::stringstream sstream;
+    sstream << "GL ERROR " << error << ": \"" << msg << "\"" << std::endl;
+    throw std::runtime_error(sstream.str());
+  }
+}
+
+void createShaderProgram()
+{
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+
+	static const char* vsSrc = R"EOF(
+
+    #version 130
+    in vec3 vertexPosition_modelspace;
+    in vec2 uvIn;
+    out vec2 uv;
+    void main()
+    {
+      gl_Position.xyz = vertexPosition_modelspace;
+      gl_Position.w = 1.0;
+      uv = uvIn;
+    }
+
+  )EOF";
+
+  static const char* fsSrc = R"EOF(
+
+    #version 130
+    out vec3 color;
+    in vec2 uv;
+
+    uniform sampler2D textureSampler;
+
+    void main()
+    {
+      color = texture(textureSampler, uv).rgb;
+    }
+
+  )EOF";
+
+  compileShader(vs, vsSrc);
+  compileShader(fs, fsSrc);
+
+	GLuint prog = glCreateProgram();
+	glAttachShader(prog, vs);
+	glAttachShader(prog, fs);
+
+  glBindAttribLocation(prog, 0, "vertexPosition_modelspace");
+  glBindAttribLocation(prog, 1, "uvIn");
+
+	glLinkProgram(prog);
+
+	printProgramLog(prog);
+
+	glDetachShader(prog, vs);
+	glDetachShader(prog, fs);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	glUseProgram(prog);
+	throwGlError("Failed to create shader program");
+}
+
+GLuint createVao(const std::vector<GLfloat>& data)
+{
+  GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * data.size(), data.data(), GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (char*)(sizeof(GLfloat) * 3));
+
+  throwGlError("Failed to create square VAO");
+  return vao;
+}
+
+GLuint createSquareVao()
+{
+  return createVao
+  ({
+    -1.f,  -1.f,  0.0f, 0.f, 0.f,
+    1.f,   -1.f,  0.0f, 1.f, 0.f,
+    -1.f,  1.f,   0.0f, 0.f, 1.f,
+    1.f,   1.f,   0.0f, 1.f, 1.f
+  });
+}
+
+GLuint createRectangleVao(float uvTop, float uvHeight)
+{
+  float uvBot = uvTop - uvHeight;
+  return createVao
+  ({
+    -1.f,  -1.f,  0.0f, 0.f, uvBot,
+    1.f,   -1.f,  0.0f, 1.f, uvBot,
+    -1.f,  1.f,   0.0f, 0.f, uvTop,
+    1.f,   1.f,   0.0f, 1.f, uvTop
+  });
+}
+
 
 int main(int, char**) {
 // Setup SDL
@@ -33,6 +180,9 @@ int main(int, char**) {
   glEnable(GL_DEPTH_TEST);
 
 
+  createShaderProgram();
+  GLuint vao = createSquareVao();
+
   bool done = false;
   while (!done)
   {
@@ -57,13 +207,17 @@ int main(int, char**) {
       }
       }
     }
+
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    SDL_GL_SwapWindow(window);
    
     // Rendering
     glViewport(0, 0, 800, 800);
     glClearColor(255, 0, 255, 255);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    SDL_GL_SwapWindow(window);
   }
 
   SDL_GL_DeleteContext(gl_context);
